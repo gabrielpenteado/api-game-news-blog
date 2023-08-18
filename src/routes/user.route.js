4b9253fe1985bd4c34a1df723c480b4aeae6d707
Gabriel Penteado <gabripenteado@gmail.com>
2023-08-18 16:32:53 -0300

    api for version 1 and 2 of game-news-blog repo

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,7 +7,7 @@ const { validId, validUser } = require("../middlewares/global.middlewares")
 
 router.post("/", userController.create);
 router.get("/", userController.findAll);
-router.get("/:id", authMiddleware, userController.findById);
+router.get("/:id", validId, validUser, userController.findById);
 router.patch("/:id", validId, validUser, userController.update);
 
 module.exports = router;
