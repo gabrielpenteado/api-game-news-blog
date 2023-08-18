779b551e14d0c5770a050829b41ee28398e68eb6
Gabriel Penteado <gabripenteado@gmail.com>
2023-08-18 16:12:23 -0300

    use authMiddleware on user findById route to get userLogged data

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 
 const userController = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const { validId, validUser } = require("../middlewares/global.middlewares")
 
 router.post("/", userController.create);
 router.get("/", userController.findAll);
-router.get("/:id", validId, validUser, userController.findById);
+router.get("/:id", authMiddleware, userController.findById);
 router.patch("/:id", validId, validUser, userController.update);
 
 module.exports = router;
