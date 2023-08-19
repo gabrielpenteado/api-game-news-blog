c44836dd1e83d444baf5d4bdfcf4f264323e6aa2
Gabriel Penteado <gabripenteado@gmail.com>
2023-08-19 13:52:56 -0300

    use authMiddleware on user routes to get userLogged data

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
 const validId = (req, res, next) => {
-  const id = req.params.id;
+  // const id = req.params.id;
+  const id = req.userId;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ message: "Invalid ID." });
@@ -12,15 +13,17 @@ const validId = (req, res, next) => {
 };
 
 const validUser = async (req, res, next) => {
-  const id = req.params.id;
-  const user = await userService.findById(id);
+  // const id = req.params.id;
+  // const id = req.userId;
+  // const user = await userService.findById(id);
+  const user = req.user;
 
   if (!user) {
     return res.status(400).json({ message: "User not found." });
   }
 
-  req.id = id;
-  req.user = user;
+  // req.id = id;
+  // req.user = user;
 
   next();
 };
